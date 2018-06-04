d93ddcf60cdf80acd6659beaa40241109faa80ec
DecstarG <33468531+DecstarG@users.noreply.github.com>
2018-06-04 11:43:52 +1000

    Updated with Build 2
    This build features an automated scam blocking feature (currently working with most Microsoft Scams, main test website being https://ms-itsupport.com/) It also includes HTML reading but not rendering yet.

--- krypto-source/Kryptonite/Form1.cs
+++ krypto-source/Kryptonite/Form1.cs
@@ -7,11 +7,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using System.Net.Http;
 
 namespace Kryptonite
 {
     public partial class Form1 : Form
     {
+        public string currentCode;
+
         public Form1()
         {
             InitializeComponent();
@@ -20,9 +24,20 @@ namespace Kryptonite
 
         private void textBox1_KeyDown(object sender, KeyEventArgs e)
         {
-            if (e.KeyCode == Keys.Enter)
+            if (e.KeyCode == Keys.Enter) //Checks for Enter/Return key press while inside TextBox1
             {
-                
+                using (WebClient client = new WebClient()) { //Sets up WebClient for request
+                    currentCode = client.DownloadString(textBox1.Text);
+                    if (currentCode.Contains("contact")&& (currentCode.Contains("microsoft")|| currentCode.Contains("ms"))&& (currentCode.Contains("13 20 58")==false|| currentCode.Contains("13-20-58") == false || currentCode.Contains("132058") == false)) //Filter algorithm *Currently only filters out Microsoft Tech Support Scams*
+                    {
+                        //Likely Tech-Support Scammer
+                        richTextBox1.Text = "Likely tech support scam. This code may be malicious or attempt to decieve users. (Results by Kryptonite(c) ScamBlock";
+                    }
+                    else {
+                        richTextBox1.Text = currentCode;
+                    }
+                    
+                }
             }
         }
     }
